55910ecf65329774cfd0afe476c106f918e905b9
johnfn <johnfn@gmail.com>
2011-03-25 16:24:58 -0700

    fix for dovod

--- settings.py
+++ settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'canvases.urls'
 
 TEMPLATE_DIRS = (
-    '/home/grant/canvases/templates/',
+    'templates/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
